1577e614078c294f836311e1c8cac535bc9b91db
stephentuso <tusodev@gmail.com>
2017-07-30 17:08:01 -0400

    Use class syntax

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const Subsume = require('subsume-limited')
 
-function MultiFilesAdapter (options) {
-  this._subsume = new Subsume(options.id)
-  this._adapters = options.adapters
-  this._defaultAdapter = options.defaultAdapter
-}
+class MultiFilesAdapter {
+  constructor (options) {
+    this._subsume = new Subsume(options.id)
+    this._adapters = options.adapters
+    this._defaultAdapter = options.defaultAdapter
+  }
 
-MultiFilesAdapter.prototype.createFile = function (filename, data, contentType) {
-  return this._getAdapter(filename).createFile(filename, data, contentType)
-}
+  createFile (filename, data, contentType) {
+    return this._getAdapter(filename).createFile(filename, data, contentType)
+  }
 
-MultiFilesAdapter.prototype.deleteFile = function (filename) {
-  return this._getAdapter(filename).deleteFile(filename)
-}
+  deleteFile (filename) {
+    return this._getAdapter(filename).deleteFile(filename)
+  }
 
-MultiFilesAdapter.prototype.getFileData = function (filename) {
-  return this._getAdapter(filename).getFileData(filename)
-}
+  getFileData (filename) {
+    return this._getAdapter(filename).getFileData(filename)
+  }
 
-MultiFilesAdapter.prototype.getFileLocation = function (config, filename) {
-  return this._getAdapter(filename).getFileLocation(config, filename)
-}
+  getFileLocation (config, filename) {
+    return this._getAdapter(filename).getFileLocation(config, filename)
+  }
 
-MultiFilesAdapter.prototype._getAdapter = function (filename) {
-  const key = this._subsume.parse(filename).data || this._defaultAdapter
-  return this._adapters[key]
+  _getAdapter (filename) {
+    const key = this._subsume.parse(filename).data || this._defaultAdapter
+    return this._adapters[key]
+  }
 }
 
 module.exports = MultiFilesAdapter
